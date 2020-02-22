bc242f25974e42ba5c45f8905662d3300bc6ba22
John Samuel <jsamchineme@gmail.com>
2020-02-22 11:51:20 +0100

    add login and jwt token verifier

---
+++ src/constants/response.ts
@@ -0,0 +1,14 @@
+export const TOKEN_EXPIRTY_TIME = '24h';
+
+export const ERROR_CODES = {
+  USR_07: {
+    message: 'account not found',
+    status: 404,
+    code: 'USR_07'
+  },
+  AUTH_01: {
+    message: 'invalid token',
+    status: 403,
+    code: 'AUTH_01'
+  },
+}
